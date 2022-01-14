f80fb00e670becb5596c1086a8162b1015d670c0
Sumon Hossain <sumonece15@gmail.com>
2022-01-14 22:36:29 +0600

    Kotlin Input

---
+++ Kotlin Standard Input-Output/Kotlin Input/src/Kotlin Input.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    println("Enter your name")
+    val name = readLine()
+    println("Enter your age")
+    var age: Int =Integer.valueOf(readLine())
+    println("Your name is $name and your age is $age")
+}  
