73d643d6f6800cdc8ac6201dee0bebcfe7733027
Sumon Hossain <sumonece15@gmail.com>
2022-01-14 22:11:36 +0600

    Logical Operator

---
+++ Kotlin_Operators/Logical Operator/src/Logical Operator.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    var a=10
+    var b=5
+    var c=15
+    var flag = false
+    var result: Boolean
+    result = (a>b) && (a>c)
+    println("(a>b) && (a>c) :"+ result)
+    result = (a>b) || (a>c)
+    println("(a>b) || (a>c) :"+ result)
+    result = !flag
+    println("!flag :"+ result)
+
+}  
