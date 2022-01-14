c9f049ca545c0e2e4cd204a2ffafa11096c191d9
Sumon Hossain <sumonece15@gmail.com>
2022-01-14 22:12:58 +0600

    Unary Operator

---
+++ Kotlin_Operators/Unary Operator/src/Unary Operator.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>){
+    var a=10
+    var b=5
+    var flag = true
+    println("+a :"+ +a)
+    println("-b :"+ -b)
+    println("++a :"+ ++a)
+    println("--b :"+ --b)
+    println("!flag :"+ !flag)
+}  
