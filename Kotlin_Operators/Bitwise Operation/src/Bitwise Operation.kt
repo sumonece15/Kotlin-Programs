5849e5379b92ff7631afc9a793527e704131266b
Sumon Hossain <sumonece15@gmail.com>
2022-01-14 22:10:20 +0600

    Bitwise Operator

---
+++ Kotlin_Operators/Bitwise Operation/src/Bitwise Operation.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    var a=10
+    var b=2
+
+    println("a.shl(b): "+a.shl(b))
+    println("a.shr(b): "+a.shr(b))
+    println("a.ushr(b:) "+a.ushr(b))
+    println("a.and(b): "+a.and(b))
+    println("a.or(b): "+a.or(b))
+    println("a.xor(b): "+a.xor(b))
+    println("a.inv(): "+a.inv())
+
+}
