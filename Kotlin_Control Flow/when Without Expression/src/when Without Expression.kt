2a4f6af86ecd4e5d23902ae4c2c1d51834aa728c
Sumon Hossain <sumonece15@gmail.com>
2022-01-15 20:45:26 +0600

    when without Expression

---
+++ Kotlin_Control Flow/when Without Expression/src/when Without Expression.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+
+    var number = 4
+    when(number) {
+        1 -> println("One")
+        2 -> println("Two")
+        3 -> println("Three")
+        4 -> println("Four")
+        5 -> println("Five")
+        else -> println("invalid number")
+    }
+
+}  
