b2597abc47535ec0682936d2e45bed591c0d07f9
Sumon Hossain <sumonece15@gmail.com>
2022-01-15 20:44:44 +0600

    when using Braces

---
+++ Kotlin_Control Flow/when Using Braces/src/when Using Braces.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>){
+    var number = 1
+    when(number) {
+        1 -> {
+            println("Monday")
+            println("First day of the week")
+        }
+        7 -> println("Sunday")
+        else -> println("Other days")
+    }
+}  
