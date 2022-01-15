a2efecca6a6dd832600d6c16053160ded824ede6
Sumon Hossain <sumonece15@gmail.com>
2022-01-15 20:25:39 +0600

    If Else Expression

---
+++ Kotlin_Control Flow/If Else Expression/src/If Else Expression.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    val num1 = 10
+    val num2 =20
+    val result = if (num1 > num2) {
+        "$num1 is greater than $num2"
+    } else {
+        "$num1 is smaller than $num2"
+    }
+    println(result)
+}  
