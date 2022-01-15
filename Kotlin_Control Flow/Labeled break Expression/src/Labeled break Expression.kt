68b01cf31a8a3faae9c3933e6a19fab7eab38a16
Sumon Hossain <sumonece15@gmail.com>
2022-01-15 23:03:08 +0600

    Labeled Break Expression

---
+++ Kotlin_Control Flow/Labeled break Expression/src/Labeled break Expression.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    loop@ for (i in 1..3) {
+        for (j in 1..3) {
+            println("i = $i and j = $j")
+            if (i == 2)
+                break@loop
+        }
+    }
+}  
