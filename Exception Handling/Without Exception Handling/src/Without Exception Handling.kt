dccd66572556d8daa7d01db3a3eb5b78ca554eb0
Sumon Hossain <sumonece15@gmail.com>
2022-01-25 00:24:14 +0600

    Without Exception Handling

---
+++ Exception Handling/Without Exception Handling/src/Without Exception Handling.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    val data = 20 / 0   //may throw exception
+    println("code below exception ...")
+
+}
+
+/*
+This above program generates an exception, which causes rest of code below the exception not executable.
+*/
