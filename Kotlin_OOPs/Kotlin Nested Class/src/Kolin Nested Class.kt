d82fe11ae49faccd1a266110a14cf848cbfac603
Sumon Hossain <sumonece15@gmail.com>
2022-02-23 12:59:04 +0600

    Kotlin Nested Class

---
+++ Kotlin_OOPs/Kotlin Nested Class/src/Kolin Nested Class.kt
@@ -0,0 +1,17 @@
+class outerClass{
+    private var name: String = "Sumon"
+    class nestedClass{
+        var description: String = "code inside nested class"
+        private var id: Int = 101
+        fun foo(){
+            //  print("name is ${name}") // cannot access the outer class member
+            println("Id is ${id}")
+        }
+    }
+}
+fun main(args: Array<String>){
+// nested class must be initialize
+    println(outerClass.nestedClass().description) // accessing property
+    var obj = outerClass.nestedClass() // object creation
+    obj.foo() // access member function
+}
