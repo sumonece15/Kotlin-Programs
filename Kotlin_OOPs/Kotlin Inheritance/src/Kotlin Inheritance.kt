7f7369da5a21cfc5a39806d1d2113236f969450f
Sumon Hossain <sumonece15@gmail.com>
2022-02-24 20:37:48 +0600

    Kotlin Inheritance

---
+++ Kotlin_OOPs/Kotlin Inheritance/src/Kotlin Inheritance.kt
@@ -0,0 +1,23 @@
+open class Employee(name: String, age: Int, salary: Float) {
+    init {
+        println("Name is $name.")
+        println("Age is $age")
+        println("Salary is $salary")
+    }
+}
+class Programmer(name: String, age: Int, salary: Float):Employee(name,age,salary){
+    fun doProgram() {
+        println("programming is my passion.")
+    }
+}
+class Salesman(name: String, age: Int, salary: Float):Employee(name,age,salary){
+    fun fieldWork() {
+        println("travelling is my hobby.")
+    }
+}
+fun main(args: Array<String>){
+    val obj1 = Programmer("Sumon", 25, 40000f)
+    obj1.doProgram()
+    val obj2 = Salesman("Sagor", 24, 30000f)
+    obj2.fieldWork()
+}
