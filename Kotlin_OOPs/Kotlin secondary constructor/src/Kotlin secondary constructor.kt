2b53076740102b4598807bcd52727ebc66a81d21
Sumon Hossain <sumonece15@gmail.com>
2022-02-24 19:30:27 +0600

    Kotlin secondary constructor

---
+++ Kotlin_OOPs/Kotlin secondary constructor/src/Kotlin secondary constructor.kt
@@ -0,0 +1,11 @@
+class myClass{
+
+    constructor(name: String, id: Int){
+        println("Name = ${name}")
+        println("Id = ${id}")
+    }
+}
+fun main(args: Array<String>){
+    val myclass = myClass ("Sumon", 101)
+
+}
