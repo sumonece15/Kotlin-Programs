2918f9da778aa01c71689b998ffe00d66125800a
Sumon Hossain <sumonece15@gmail.com>
2022-03-27 14:29:11 +0600

    Update Kotlin String Example 1.kt

--- Kotlin_String/Kotlin String Example 1/src/Kotlin String Example 1.kt
+++ Kotlin_String/Kotlin String Example 1/src/Kotlin String Example 1.kt
@@ -4,7 +4,7 @@
 The characters which are present in string are known as elements of string.
 Element of string are accessed by indexing operation string[index].
 String's index value starts from 0 and ends at one less than the size of string string[string.length-1].
-Index 0 represent first element, index 1 represent second element and so on...
+Index 0 represent first element, index 1 represent second element and so on.
 */
 
 fun main(args: Array<String>) {
